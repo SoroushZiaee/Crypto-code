4dad999af13d9d13848222d7af050e6418efd8ab
Soroush Ziaee <32066123+SoroushZiaee@users.noreply.github.com>
2021-09-04 00:05:55 +0430

    Some A little Change on csv file and the code

--- phase1/get_data.py
+++ phase1/get_data.py
@@ -10,13 +10,14 @@ def return_timestamp(data):
 
 client = Client(config.API_KEY, config.API_SECRET, config.API_PASSPHRASE)
 
-candles = client.get_kline_data(symbol='BTC-USDT', kline_type='5min', start=return_timestamp('2017-01-01'), end=return_timestamp('2018-01-05'))[::-1]
+candles = client.get_kline_data(symbol='BTC-USDT', kline_type='5min')[::-1]
 
-with open('5minutes.csv', 'w', newline='') as fout:
+with open('5minutes-kucoin.csv', 'w', newline='') as fout:
     candlestick_writer = csv.writer(fout, delimiter=',')
     for candle in candles:
         candlestick_writer.writerow(candle)
 
 print(len(candles))
+
 
 
---
+++ phase1/yfinance-tutorial.py
@@ -0,0 +1,8 @@
+import numpy as np 
+import yfinance as yf
+import plotly.graph_objects as go
+
+
+data = yf.download('BTC-USD', period='1d', interval='5m')
+
+data.to_csv('5min-yfinance.csv', sep=',')
