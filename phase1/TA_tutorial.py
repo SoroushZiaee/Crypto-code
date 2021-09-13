34536108798e1b94568a9be596c27f379130f387
Soroush Ziaee <32066123+SoroushZiaee@users.noreply.github.com>
2021-09-14 02:12:30 +0430

    Create The Indicators and Oscillators with plot

--- phase1/TA_tutorial.py
+++ phase1/TA_tutorial.py
@@ -1,21 +1,2 @@
-import numpy as np
-import talib
-import matplotlib.pyplot as plt
-
-# close = np.random.random(100)
-# print(close)
-
-# # Calculate the moving Average
-# moving_average = talib.SMA(close, timeperiod=10)
-# print(moving_average)
-
-my_data = np.genfromtxt('15minutes.csv', delimiter=',')
-close = my_data[:, 4]
-print(close)
-
-rsi = talib.RSI(close, timeperiod=5)
-print(rsi)
-
-plt.plot(rsi)
-plt.grid(True)
-plt.show()
+import talib as ta
+import pandas as pd
