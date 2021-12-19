4f835d7363f14e6603cdf8d03345efb619a8c9e4
Soroush Ziaee <32066123+SoroushZiaee@users.noreply.github.com>
2021-12-20 01:24:34 +0330

    previous update on phase1

--- phase1/yfinance-tutorial.py
+++ phase1/yfinance-tutorial.py
@@ -3,6 +3,6 @@ import yfinance as yf
 import plotly.graph_objects as go
 
 
-data = yf.download('BTC-USD', period='1d', interval='5m')
+data = yf.download('BTC-USD',period='1d', interval='5m')
 
 data.to_csv('5min-yfinance.csv', sep=',')
